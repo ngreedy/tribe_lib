43a02ca3ae108bb715a8e2b7f66cf1f77d4915b3
侯晶南 <hjn910@163.com>
2017-03-29 12:00:05 +0800

    set

---
+++ app/src/main/java/com/gs/buluo/tribalclib/MainActivity.java
@@ -0,0 +1,17 @@
+package com.gs.buluo.tribalclib;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by hjn on 2017/3/29.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
