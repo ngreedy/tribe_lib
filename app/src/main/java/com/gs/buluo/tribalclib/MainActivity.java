e5c271a70b2cd390e1743af2466773bd09bcf71b
侯晶南 <hjn910@163.com>
2017-03-30 18:04:29 +0800

    2.0

--- app/src/main/java/com/gs/buluo/tribalclib/MainActivity.java
+++ app/src/main/java/com/gs/buluo/tribalclib/MainActivity.java
@@ -2,7 +2,6 @@ package com.gs.buluo.tribalclib;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
 /**
  * Created by hjn on 2017/3/29.
@@ -10,7 +9,7 @@ import android.support.annotation.Nullable;
 
 public class MainActivity extends Activity{
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
