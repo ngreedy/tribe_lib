6f7d76385dc860e72415b519b730e6fea2020fb3
侯晶南 <hjn910@163.com>
2017-05-16 11:58:54 +0800

    reset dialog

--- common/src/main/java/com/gs/buluo/common/network/BaseSubscriber.java
+++ common/src/main/java/com/gs/buluo/common/network/BaseSubscriber.java
@@ -25,56 +25,28 @@ import rx.Subscriber;
 public abstract class BaseSubscriber<T> extends Subscriber<T> {
     private static final String TAG = "Network";
     private boolean showLoading = true;
-    private WindowManager mWindowManager;
-    private View mFloatLayout;
-    private WindowManager.LayoutParams wmParams;
 
     public BaseSubscriber(boolean showLoading) {
         this.showLoading = showLoading;
     }
 
     public BaseSubscriber() {
-        wmParams = new WindowManager.LayoutParams();
-//获取的是WindowManagerImpl.CompatModeWrapper
-        mWindowManager = (WindowManager) BaseApplication.getInstance().getSystemService(BaseApplication.getInstance().WINDOW_SERVICE);
-//设置window type
-        wmParams.type = WindowManager.LayoutParams.TYPE_PHONE;
-//设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
-        wmParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-//调整悬浮窗显示的停靠位置为左侧置顶
-        wmParams.gravity = Gravity.LEFT | Gravity.TOP;
-// 以屏幕左上角为原点，设置x、y初始值，相对于gravity
-        wmParams.x = 0;
-        wmParams.y = 0;
-
-//设置悬浮窗口长宽数据
-        wmParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
-        wmParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
-
-        LayoutInflater inflater = LayoutInflater.from(BaseApplication.getInstance());
-//获取浮动窗口视图所在布局
-        mFloatLayout = (LinearLayout) inflater.inflate(R.layout.simple_dialog, null);
-//添加mFloatLayout
-
     }
 
     @Override
     public void onStart() {
-//        if (showLoading) BaseApplication.showDialog(R.string.loading);
-        mWindowManager.addView(mFloatLayout, wmParams);
+        if (showLoading) BaseApplication.showDialog(R.string.loading);
     }
 
     @Override
     public void onCompleted() {
-//        if (showLoading) BaseApplication.dismissDialog();
-        mFloatLayout.setVisibility(View.GONE);
+        if (showLoading) BaseApplication.dismissDialog();
     }
 
     @Override
     public void onError(Throwable e) {
         if (showLoading) {
-//            BaseApplication.dismissDialog();
-            mFloatLayout.setVisibility(View.GONE);
+            BaseApplication.dismissDialog();
         }
         if (e instanceof HttpException) {       //http返回异常
             //http error
