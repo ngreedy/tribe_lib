d98db25347b29906b05fcafb1c60044da56fd8ca
侯晶南 <hjn910@163.com>
2017-05-27 17:07:36 +0800

    dialog 改版

--- common/src/main/java/com/gs/buluo/common/widget/LoadingDialog.java
+++ common/src/main/java/com/gs/buluo/common/widget/LoadingDialog.java
@@ -1,11 +1,21 @@
 package com.gs.buluo.common.widget;
 
-import android.app.ProgressDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.gs.buluo.common.R;
 
 public class LoadingDialog {
 
-	private ProgressDialog mDialog;
+	private Dialog mDialog;
 	private static LoadingDialog instance = null;
 
 	public static synchronized LoadingDialog getInstance() {
@@ -15,15 +25,38 @@ public class LoadingDialog {
 		return instance;
 	}
 	
-	public void show(Context context, String message, boolean cancleable){
-		mDialog =  ProgressDialog.show(context, null, message);
-		mDialog.setCancelable(cancleable);
+	public void show(Context context, String message, boolean cancelable){
+		mDialog =  createLoadingDialog(context, message, cancelable);
 		mDialog.show();
 	}
-	public void show(Context context, int message, boolean cancleable){
-		mDialog =  ProgressDialog.show(context, null, context.getResources().getString(message));
-		mDialog.setCancelable(cancleable);
+	public void show(Context context, int message, boolean cancelable){
+		mDialog =  createLoadingDialog(context,  context.getResources().getString(message),cancelable);
 		mDialog.show();
+	}
+	public Dialog createLoadingDialog(Context context, String msg ,boolean cancelable) {
+
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View v = inflater.inflate(R.layout.dialog_my, null);// 得到加载view
+		LinearLayout layout = (LinearLayout) v.findViewById(R.id.dialog_view);// 加载布局
+		// main.xml中的ImageView
+
+		ImageView spaceshipImage = (ImageView) v.findViewById(R.id.img);
+		TextView tipTextView = (TextView) v.findViewById(R.id.tipTextView);// 提示文字
+		// 加载动画
+		Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(
+				context, R.anim.loading_animation);
+		// 使用ImageView显示动画
+		hyperspaceJumpAnimation.setInterpolator(new LinearInterpolator());
+		spaceshipImage.startAnimation(hyperspaceJumpAnimation);
+		tipTextView.setText(msg);// 设置加载信息
+
+		Dialog loadingDialog = new Dialog(context, R.style.loading_dialog);// 创建自定义样式dialog
+
+		loadingDialog.setCancelable(cancelable);// 不可以用“返回键”取消
+		loadingDialog.setContentView(layout, new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.FILL_PARENT,
+				LinearLayout.LayoutParams.FILL_PARENT));// 设置布局
+		return loadingDialog;
 	}
 
 	public void dismissDialog() {
