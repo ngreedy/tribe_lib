ad58f8f2881089d29a46d6943f36e769f210ae7f
ngreedy <hjn910@163.com>
2018-03-20 17:20:08 +0800

    status layout 登录状态 fix

--- common/src/main/java/com/gs/buluo/common/widget/StatusLayout.java
+++ common/src/main/java/com/gs/buluo/common/widget/StatusLayout.java
@@ -78,6 +78,8 @@ public class StatusLayout extends FrameLayout {
 
         progressView.setVisibility(View.GONE);
 
+        loginView.setVisibility(View.GONE);
+
         currentShowingView = contentView;
     }
 
