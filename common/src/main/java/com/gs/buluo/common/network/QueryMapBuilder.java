8e24d6613612079c82acc69f216e61d812da31a7
ngreedy <hjn910@163.com>
2018-03-21 18:50:37 +0800

    putAll  putObject

--- common/src/main/java/com/gs/buluo/common/network/QueryMapBuilder.java
+++ common/src/main/java/com/gs/buluo/common/network/QueryMapBuilder.java
@@ -1,6 +1,11 @@
 package com.gs.buluo.common.network;
 
+import android.text.TextUtils;
 import android.util.Base64;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by hjn91 on 2018/1/30.
@@ -25,6 +30,47 @@ public class QueryMapBuilder {
         if (value == null) value = "";
         sortedMap.put(key, value);
         return builder;
+    }
+
+    public QueryMapBuilder putAll(Map map) {
+        sortedMap.putAll(map);
+        return builder;
+    }
+
+
+    public QueryMapBuilder putObject(Object object) {
+        sortedMap.putAll(convertObjToMap(object));
+        return builder;
+    }
+
+
+    public Map<String, String> convertObjToMap(Object obj) {
+        Map<String, String> reMap = new HashMap<>();
+        if (obj == null)
+            return null;
+        Field[] fields = obj.getClass().getDeclaredFields();
+        try {
+            for (int i = 0; i < fields.length; i++) {
+                try {
+                    String fieldName = fields[i].getName();
+                    Field f = obj.getClass().getDeclaredField(fieldName);
+                    f.setAccessible(true);
+                    Object o = f.get(obj);
+                    if (o != null && !fieldName.contains("$") && !TextUtils.equals("CREATOR", fieldName) && !TextUtils.equals(fieldName, "serialVersionUID")) {
+                        reMap.put(fieldName, o.toString());
+                    }
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        return reMap;
     }
 
     public SortedTreeMap<String, String> buildGet() {
