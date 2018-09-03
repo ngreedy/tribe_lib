ca30ccbd1ec8278da80469a66b9671164857bf15
ngreedy <hjn910@163.com>
2018-09-03 14:28:02 +0800

    observe to DisposableObserver

--- common/src/main/java/com/gs/buluo/common/network/BaseSubscriber.java
+++ common/src/main/java/com/gs/buluo/common/network/BaseSubscriber.java
@@ -16,36 +16,30 @@ import java.io.IOException;
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 import retrofit2.HttpException;
 
 /**
  * Created by hjn on 2017/3/20.
  */
 
-public abstract class BaseSubscriber<T> implements Observer<T> {
+public abstract class BaseSubscriber<T> extends DisposableObserver<T> {
     private static final String TAG = "Network";
-
-    @Override
-    public void onSubscribe(Disposable d) {
-
-    }
 
     @Override
     public void onComplete() {
         LoadingDialog.getInstance().dismissDialog();
     }
 
-
     @Override
     public void onError(Throwable e) {
-        Log.e(TAG, "onError: " + e);
         LoadingDialog.getInstance().dismissDialog();
         if (e instanceof HttpException) {       //http返回异常
-            onFail(new ApiException(((HttpException) e).code(), BaseApplication.getInstance().getApplicationContext().getResources().getString(R.string.connect_fail), "HttpException"));
+            onFail(new ApiException(((HttpException) e).code(), BaseApplication.getInstance().getApplicationContext().getResources().getString(R.string.http_fail), "HttpException"));
         } else if (e instanceof IOException) {
-            onFail(new ApiException(554, BaseApplication.getInstance().getApplicationContext().getResources().getString(R.string.connect_fail), "IOException"));
+            onFail(new ApiException(555, BaseApplication.getInstance().getApplicationContext().getResources().getString(R.string.connect_fail), "IOException"));
         } else if (e instanceof JSONException || e instanceof JsonParseException) {
-            onFail(new ApiException(554, BaseApplication.getInstance().getApplicationContext().getResources().getString(R.string.convert_fail), "JSONException"));
+            onFail(new ApiException(555, BaseApplication.getInstance().getApplicationContext().getResources().getString(R.string.convert_fail), "JSONException"));
         } else if (e instanceof ApiException) {
             ApiException exception = (ApiException) e;
             if (exception.getCode() == 10003 || exception.getCode() == 10011) {
